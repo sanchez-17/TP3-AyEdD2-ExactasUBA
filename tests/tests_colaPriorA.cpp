974c2abd1a161b4382a823c6253e44eb3d7bb6bf
sanchez-17 <gasanchez@dc.uba.ar>
2023-07-02 01:38:47 -0300

    colaPriorA:test Fix-punterosTest corregido

--- tests/tests_colaPriorA.cpp
+++ tests/tests_colaPriorA.cpp
@@ -122,11 +122,43 @@ TEST(colaPriorATest, desencolarMayor) {
     tuplaPersona<Nat, Persona> per3(300,3);
     tuplaPersona<Nat, Persona> per4(400,4);
     tuplaPersona<Nat, Persona> per5(500,5);
-    q.encolar(per1);
+    q.encolar(per5);
+    q.encolar(per4);
     q.encolar(per3);
     q.encolar(per2);
-    q.encolar(per4);
+    q.encolar(per1);
+    ASSERT_TRUE(q.proximo().getGastoPersona()==500);
+    ASSERT_TRUE(q.proximo().getPersona() == 5);
+    ASSERT_FALSE(q.vacia());
+    q.desencolar();
+    ASSERT_TRUE(q.proximo().getPersona() == 4 && q.proximo().getGastoPersona()==400);
+    ASSERT_FALSE(q.vacia());
+    q.desencolar();
+    ASSERT_TRUE(q.proximo().getPersona() == 3 && q.proximo().getGastoPersona()==300);
+    ASSERT_FALSE(q.vacia());
+    q.desencolar();
+    ASSERT_TRUE(q.proximo().getPersona() == 2 && q.proximo().getGastoPersona()==200);
+    ASSERT_FALSE(q.vacia());
+    q.desencolar();
+    ASSERT_TRUE(q.proximo().getPersona() == 1 && q.proximo().getGastoPersona()==100);
+    ASSERT_FALSE(q.vacia());
+    q.desencolar();
+    ASSERT_TRUE(q.vacia());
+}
+
+TEST(colaPriorATest,desencolarRandom){
+    colaPriorA<Nat,Persona> q(5);
+    ASSERT_TRUE(q.vacia());
+    tuplaPersona<Nat, Persona> per1(100,1);
+    tuplaPersona<Nat, Persona> per2(200,2);
+    tuplaPersona<Nat, Persona> per3(300,3);
+    tuplaPersona<Nat, Persona> per4(400,4);
+    tuplaPersona<Nat, Persona> per5(500,5);
+    q.encolar(per3);
+    q.encolar(per1);
     q.encolar(per5);
+    q.encolar(per2);
+    q.encolar(per4);
     ASSERT_TRUE(q.proximo().getGastoPersona()==500);
     ASSERT_TRUE(q.proximo().getPersona() == 5);
     ASSERT_FALSE(q.vacia());
@@ -178,37 +210,38 @@ TEST(colaPriorATest, punterosCorrectos) {
 }
 
 TEST(colaPriorATest, punterosCorrectos_valores_modificados) {
+    //Todas las personas compran 2 veces(aumentan sus gastos acumulados)
     colaPriorA<Nat, Persona> q(5);
     ASSERT_TRUE(q.vacia());
     //encolo todas las personas
-    tuplaPersona<Nat, Persona> per1(0,1);
-    tuplaPersona<Nat, Persona> per2(0,7);
-    tuplaPersona<Nat, Persona> per3(0,3);
-    tuplaPersona<Nat, Persona> per4(0,4);
-    tuplaPersona<Nat, Persona> per5(0,5);
-    Nat* puntero = q.encolar(per5);
+    tuplaPersona<Nat, Persona> per1(100,1);
+    tuplaPersona<Nat, Persona> per2(200,7);
+    tuplaPersona<Nat, Persona> per3(300,3);
+    tuplaPersona<Nat, Persona> per4(500,4);
+    tuplaPersona<Nat, Persona> per5(700,5);
+    Nat* puntero5 = q.encolar(per5);
     Nat* puntero4 = q.encolar(per4);
     Nat* puntero2 = q.encolar(per2);
     Nat* puntero3 = q.encolar(per3);
     Nat* puntero1 = q.encolar(per1);
-    EXPECT_EQ(*puntero, 0);
-    EXPECT_EQ(*puntero4, 0);
-    EXPECT_EQ(*puntero2, 0);
-    EXPECT_EQ(*puntero3, 0);
-    EXPECT_EQ(*puntero1, 0);
-    tuplaPersona<Nat, Persona> per10(100,1);
-    tuplaPersona<Nat, Persona> per20(200,7);
-    tuplaPersona<Nat, Persona> per30(300,3);
-    tuplaPersona<Nat, Persona> per40(400,4);
-    tuplaPersona<Nat, Persona> per50(500,5);
+    EXPECT_EQ(*puntero1, 100);
+    EXPECT_EQ(*puntero4, 500);
+    EXPECT_EQ(*puntero2, 200);
+    EXPECT_EQ(*puntero3, 300);
+    EXPECT_EQ(*puntero5, 700);
+    tuplaPersona<Nat, Persona> per10(700,1);
+    tuplaPersona<Nat, Persona> per20(300,7);
+    tuplaPersona<Nat, Persona> per30(400,3);
+    tuplaPersona<Nat, Persona> per40(600,4);
+    tuplaPersona<Nat, Persona> per50(800,5);
     Nat* puntero10 = q.encolar(per10);
     Nat* puntero20 = q.encolar(per20);
     Nat* puntero30 = q.encolar(per30);
     Nat* puntero40 = q.encolar(per40);
     Nat* puntero50 = q.encolar(per50);
-    EXPECT_EQ(*puntero10, 100);
-    EXPECT_EQ(*puntero20, 200);
-    EXPECT_EQ(*puntero30, 300);
-    EXPECT_EQ(*puntero40, 400);
-    EXPECT_EQ(*puntero50, 500);
+    EXPECT_EQ(*puntero10, 700);
+    EXPECT_EQ(*puntero20, 300);
+    EXPECT_EQ(*puntero30, 400);
+    EXPECT_EQ(*puntero40, 600);
+    EXPECT_EQ(*puntero50, 800);
 }
